2405d107aba04ee088e685c91f5785e29505f330
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-02-12 03:54:39 +0600

    matrix multi by c

--- Untitled1.c
+++ Untitled1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main() {
 
-   printf("Hello, ream");
+    int arr[3];
+
+    arr[0]=1;
+    arr[1]=2;
+    arr[2]=3;
+
+    printf("print the value of arr \n %d\n %d \n %d",arr[0] ,arr[1],arr[2]);
+
    return 0;
 }
